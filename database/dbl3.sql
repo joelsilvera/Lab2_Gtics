88351f98f4421ee4217c2554fafd4ffd2b73d54c
Daniel Chomon <a20180805@pucp.edu.pe>
2022-09-13 21:56:17 -0500

    BAsededatos se crea sola

--- database/dbl3.sql
+++ database/dbl3.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `currency`
 --
-
+CREATE DATABASE dbl3;
+use dbl3;
 DROP TABLE IF EXISTS `currency`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
