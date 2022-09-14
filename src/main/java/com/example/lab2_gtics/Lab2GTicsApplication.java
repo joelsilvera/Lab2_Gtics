9db7dad2c60b2cb05ff6005ba46f81cb3b061159
joelsilvera <joel.silvera@pucp.edu.pe>
2022-09-13 20:46:13 -0500

    nuevo

---
+++ src/main/java/com/example/lab2_gtics/Lab2GTicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2GTicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2GTicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab2_gtics/Lab2GTicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2GTicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
