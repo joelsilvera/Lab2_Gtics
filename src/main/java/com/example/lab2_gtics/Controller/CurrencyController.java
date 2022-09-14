622a3e40149b59ad654c9d1bccd0e9ce26d1aa3a
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2022-09-14 09:43:13 -0500

    avance

---
+++ src/main/java/com/example/lab2_gtics/Controller/CurrencyController.java
@@ -0,0 +1,25 @@
+package com.example.lab2_gtics.controller;
+
+import com.example.lab2_gtics.Repository.CurrencyRepository;
+import com.example.lab2_gtics.entity.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/currency")
+public class CurrencyController {
+    @Autowired
+    CurrencyRepository currencyRepository;
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        List<Currency> currencyList = currencyRepository.findAll();
+        model.addAttribute("currencyList", currencyList);
+        return "principal";
+    }
+}
